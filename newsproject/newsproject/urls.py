0dc767fea7ad140286808dc394d827f07a0783cd
Abdifahtaho <112065866+Abdifahtaho@users.noreply.github.com>
2024-06-07 17:41:51 +0300

    update login

--- newsproject/newsproject/urls.py
+++ newsproject/newsproject/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('newsApp.urls'))
+    path('users/',include('users.urls')),
+    path('',include('newsApp.urls')),
+    
 ]
